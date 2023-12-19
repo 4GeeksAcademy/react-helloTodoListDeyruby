2da357212636b32ba832fa4647aa32f2ef6b1459
Dey <128061593+Deyruby@users.noreply.github.com>
2023-12-19 22:48:07 +0000

    tarea terminada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [task, setTask] = useState([])
+
+
+	const enterPressed = (event) => {
+		if (event.keyCode === 13) {
+			handleChange(event)
+
+		}
+	}
+	const handleChange = (event) => {
+		setTask([...task, event.target.value])
+		console.log(event.target.value)
+		event.target.value = ""
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="todolist">
+				<div className="header">
+					<p>to-do-list</p>
+				</div>
+				<div className="container">
+					<ul>
+						<li className="input1"><input type="text" name="tareas" placeholder="Add a new task" onKeyDown={(e) => enterPressed(e)} /></li>
+
+						{
+							task.map((value, index) => { 
+							console.log("index", index)
+							return <li className="list" key={index}>{value} 
+							<i onClick={() => setTask(task.filter((_, currentIndex) => { 
+								console.log("currentIndex", currentIndex) 
+								return currentIndex != index}))} className="fa-solid fa-trash"></i> 
+							</li>})
+
+						}
+					</ul>
+
+				</div>
+			</div>
+
+		</>
 	);
 };
 
